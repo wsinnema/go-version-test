8a58a202c674bec3a39b2aed6a6fcea865ad40bc
Wessel Sinnema <wesselsinnema1@me.com>
2016-10-10 10:23:21 +0200

    Added subpackage

---
+++ subpackage/subpackage.go
@@ -0,0 +1,7 @@
+package subpackage
+
+import "fmt"
+
+func SubPackageVersion() {
+	fmt.Println("v3.0")
+}
--- version.go
+++ version.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/wsinnema/wsinnema/go-version-test/subpackage"
+)
 
 func main() {
+	subpackage.SubPackageVersion()
 	fmt.Println("v2.0")
 }
